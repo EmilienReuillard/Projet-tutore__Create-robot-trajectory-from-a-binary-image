72653c482609e7eeb8dbc89d076fe7216d91d646
EmilienReuillard <emilien.reuillard@outlook.com>
2023-01-20 08:13:00 +0100

    épuration du main en déplaçant les fonctions dans class_graph

--- class_graph.py
+++ class_graph.py
@@ -18,6 +18,8 @@ class graph:
     
 #_______VARIABLES_______   
     coords_peaks = []
+    lst_connections = []
+    N_peaks = 0
     
 #______FONCTIONS_____
 
@@ -25,25 +27,26 @@ class graph:
         #ouverture de l'image
         self.image = io.imread(path_image)
         self.image_edges = canny(self.image,3)
-        self.image_blured = skimage.filters.gaussian(self.image_edges,(1,1))
+        self.image_blurred = skimage.filters.gaussian(self.image_edges,(1,1))
         
         #Détection des coins dans l'image
         self.coords_peaks = corner_peaks(corner_harris(self.image), min_distance=5, threshold_rel=0.02)
-
+        self.N_peaks = len(self.coords_peaks)
+        
     #Fonction Vérifie la connection entre 2 points via des lignes droites
     def connect2points(self,pt1,pt2):
-        x1 = pt1[0]
-        y1 = pt1[1]
-        x2 = pt2[0]
-        y2 = pt2[1]
+        x1 = self.coords_peaks[pt1][0]
+        y1 = self.coords_peaks[pt1][1]
+        x2 = self.coords_peaks[pt2][0]
+        y2 = self.coords_peaks[pt2][1]
         
         l = sqrt((y2-y1)**2 + (x2-x1)**2)   #distance entre les 2 points
         L = int(l)  #distance entière
         lst_pt_x = np.linspace(x1,x2,L) #liste qui regroupes les coordonées en x à chaque pas de temps
         lst_pt_y = np.linspace(y1,y2,L) #liste qui regroupes les coordonées en y à chaque pas de temps
         
-        print(f"x1={x1} y1={y1} x2={x2} y2={y2}")
-        print(f"L = {L}")
+        #print(f"x1={x1} y1={y1} x2={x2} y2={y2}")
+        #print(f"L = {L}")
         
         compt = 0
         i,j = int(lst_pt_x[0]),int(lst_pt_y[0])
@@ -53,9 +56,20 @@ class graph:
             j = int(lst_pt_y[compt])
             
             if (( i >= x2-5 and i <= x2+5 ) and (j >= y2-5 and j <= y2+5)) :
-                print(f"pt1:{pt1} & pt2:{pt2} connected")
+                #print(f"{pt1} & {pt2} connected\n")
                 return True
             
             if(self.image_blurred[i][j] == 0):
-                print(f"no connection beetween pt1:{pt1} & pt2{pt2}")
+                #print(f"no connection beetween pt1:{pt1} & pt2:{pt2}")
                 return False
+            
+    #fonction qui liste les connexions de chaque point
+    def mapping_connexions(self):
+        for i in range(self.N_peaks):
+            for j in range(self.N_peaks):
+                if i != j:
+                    if self.connect2points(i,j) == True:
+                        self.lst_connections.append([])
+                        self.lst_connections[i].append(i)
+                        self.lst_connections[i].append(j)
+
