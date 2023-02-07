ca79e5a8d3bea794e36dc2e8a9e5ff13ba931930
antoninrnr <antonin.renoir@orange.fr>
2023-02-07 14:11:29 +0100

    gg

--- main.py
+++ main.py
@@ -94,7 +94,10 @@ def main(args=None):
     #paramètres du robot
     a1 = 0.425 
     a2 = 0.345
-    
+    alpha_max = np.pi
+    alpha_min = 0.
+    beta_max = 2*np.pi/3
+    beta_min = -2*np.pi/3
     #déclaration de l'élément graph de la classe graph
     graph1 = graph("TE.png")
     l = 0.4 #selon x
@@ -109,13 +112,13 @@ def main(args=None):
     pt_t_r = [origin[0]+ l,origin[1] + h]
     pt_t_l=[origin[0], origin[1] + h]
     
-    test_b_l = is_in_workspace(pt_b_l[0], pt_b_l[1], a1,a2,0,0, -np.pi, np.pi)
-    test_b_r = is_in_workspace(pt_b_r[0], pt_b_r[1], a1,a2,0,0, -np.pi, np.pi)
-    test_t_r = is_in_workspace(pt_t_r[0], pt_t_r[1], a1,a2,0,0, -np.pi, np.pi)
-    test_t_l = is_in_workspace(pt_t_l[0], pt_t_l[1] , a1,a2,0,0, -np.pi, np.pi)
+    test_b_l = is_in_workspace(pt_b_l[0], pt_b_l[1], a1,a2,alpha_min,alpha_max, beta_min, beta_max)
+    test_b_r = is_in_workspace(pt_b_r[0], pt_b_r[1], a1,a2,alpha_min,alpha_max, beta_min, beta_max)
+    test_t_r = is_in_workspace(pt_t_r[0], pt_t_r[1], a1,a2,alpha_min,alpha_max, beta_min, beta_max)
+    test_t_l = is_in_workspace(pt_t_l[0], pt_t_l[1], a1,a2,alpha_min,alpha_max, beta_min, beta_max)
    
     if (test_b_l == True and test_t_r == True and test_t_l==True and test_b_r==True):
-        
+        print("Image OK")
         #vérification que l'image rentre deq
         
         lst = graph1.trajectory_pts_reel    #lst contient les coordonées xyz
