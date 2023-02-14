96ab239cf7513bda3922968b28435e02799831c4
antoninrnr <antonin.renoir@orange.fr>
2023-02-14 16:42:08 +0100

    kk

--- publisher_marker.py
+++ publisher_marker.py
@@ -65,35 +65,43 @@ if __name__ == '__main__':
 
     """
    
-import rclpy
 import math
+import rclpy
 from rclpy.node import Node
-from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
 
-class SinusoidalSurfacePublisher(Node):
+class SinusoidSurface(Node):
 
     def __init__(self):
-        super().__init__('sinusoidal_surface_publisher')
-        self.publisher_ = self.create_publisher(Marker, 'visualization_marker', 10)
-
-        timer_period = 0.1  # en secondes
+        super().__init__('sinusoid_surface')
+        self.publisher_ = self.create_publisher(Marker, 'sinusoid', 10)
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-
-        self.marker_id = 0
 
     def timer_callback(self):
         marker_msg = Marker()
-        marker_msg.header.frame_id = 'base_link'  # le frame_id dans lequel les points du marker sont définis
+        marker_msg.header.frame_id = 'map'
         marker_msg.header.stamp = self.get_clock().now().to_msg()
 
-        marker_msg.ns = 'sinusoidal_surface'
-        marker_msg.id = self.marker_id
-        self.marker_id += 1
+        # Set the marker type to TRIANGLE_STRIP
+        marker_msg.type = Marker.TRIANGLE_STRIP
 
-        marker_msg.type = Marker.TRIANGLE_LIST
+        # Set the marker action to ADD
         marker_msg.action = Marker.ADD
 
+        # Set the marker scale
+        marker_msg.scale.x = 0.1
+        marker_msg.scale.y = 1.0
+        marker_msg.scale.z = 1.0
+
+        # Set the marker color
+        marker_msg.color.r = 1.0
+        marker_msg.color.g = 0.0
+        marker_msg.color.b = 0.0
+        marker_msg.color.a = 1.0
+
+        # Set the marker pose
         marker_msg.pose.position.x = 0.0
         marker_msg.pose.position.y = 0.0
         marker_msg.pose.position.z = 0.0
@@ -102,32 +110,29 @@ class SinusoidalSurfacePublisher(Node):
         marker_msg.pose.orientation.z = 0.0
         marker_msg.pose.orientation.w = 1.0
 
-        marker_msg.scale.x = 1.0  # la largeur de la surface en mètres
-        marker_msg.scale.y = 0.1  # l'épaisseur de la surface en mètres
-        marker_msg.scale.z = 1.0  # la longueur de la surface en mètres
-        marker_msg.color.a = 1.0  # l'opacité du marker, 1.0 signifie complètement opaque
-        marker_msg.color.r = 1.0  # la composante rouge de la couleur
-        marker_msg.color.g = 0.0  # la composante verte de la couleur
-        marker_msg.color.b = 0.0  # la composante bleue de la couleur
-
-        # Définition des points pour le TriangleStrip
-        num_points = 100
-        marker_msg.points = [Point() for _ in range(2 * num_points)]
+        # Set the marker points
+        num_points = 50
+        marker_msg.points = []
         for i in range(num_points):
-            marker_msg.points[2*i].x = i / 10.0
-            marker_msg.points[2*i].y = 0.5 * math.sin(i / 10.0)
-            marker_msg.points[2*i].z = 0.0
-            marker_msg.points[2*i+1].x = i / 10.0
-            marker_msg.points[2*i+1].y = 0.5 * math.sin(i / 10.0) - 0.1
-            marker_msg.points[2*i+1].z = 1.0
-
+            x = i / num_points
+            y = math.sin(2 * math.pi * x)
+            z = 0.0
+            marker_point = Point()
+            marker_point.x = x
+            marker_point.y = y
+            marker_point.z = z
+            marker_msg.points.append(marker_point)
+            
         self.publisher_.publish(marker_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    sinusoidal_surface_publisher = SinusoidalSurfacePublisher()
-    rclpy.spin(sinusoidal_surface_publisher)
-    sinusoidal_surface_publisher.destroy_node()
+
+    sinusoid_surface = SinusoidSurface()
+
+    rclpy.spin(sinusoid_surface)
+
+    sinusoid_surface.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
