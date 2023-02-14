3e498fe32881ba1255126ca3f16c2c4dbed0512a
antoninrnr <antonin.renoir@orange.fr>
2023-02-14 16:17:13 +0100

    dfdf

--- publisher_marker.py
+++ publisher_marker.py
@@ -2,49 +2,59 @@ import rclpy
 from rclpy.node import Node
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
-import math
 
-class SinusoidSurfaceMarker(Node):
+class LineStripPublisher(Node):
+
     def __init__(self):
-        super().__init__('sinusoid_surface_marker')
-        self.marker_pub = self.create_publisher(Marker, 'sinusoid_surface_marker', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)
-        self.marker = Marker()
-        self.marker.header.frame_id = "world"
-        self.marker.type = Marker.POINTS
-        self.marker.action = Marker.ADD
-        self.marker.scale.x = 0.1
-        self.marker.scale.y = 0.1
-        self.marker.scale.z = 0.1
-        self.marker.color.r = 1.0
-        self.marker.color.g = 0.0
-        self.marker.color.b = 0.0
-        self.marker.color.a = 1.0
-        
-        self.num_points = 10
-        self.amplitude = 1
-        self.frequency = 2
+        super().__init__('line_strip_publisher')
+        self.publisher_ = self.create_publisher(Marker, 'visualization_marker', 10)
+
+        timer_period = 0.1  # en secondes
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.marker_id = 0
 
     def timer_callback(self):
-        self.marker.points = []
-        for i in range(self.num_points):
-            x = i * 0.1
-            y = self.amplitude * math.sin(self.frequency * x)
-            z = 0
-            point = Point()
-            point.x = float(x)
-            point.y = float(y)
-            point.z = float(z)
-            self.marker.points.append(point)
-        self.marker.header.stamp = self.get_clock().now().to_msg()
-        self.marker_pub.publish(self.marker)
-        print("OK")
+        marker_msg = Marker()
+        marker_msg.header.frame_id = 'base_link'  # le frame_id dans lequel les points du marker sont définis
+        marker_msg.header.stamp = self.get_clock().now().to_msg()
+
+        marker_msg.ns = 'line_strip'
+        marker_msg.id = self.marker_id
+        self.marker_id += 1
+
+        marker_msg.type = Marker.LINE_STRIP
+        marker_msg.action = Marker.ADD
+
+        marker_msg.pose.position.x = 0.0
+        marker_msg.pose.position.y = 0.0
+        marker_msg.pose.position.z = 0.0
+        marker_msg.pose.orientation.x = 0.0
+        marker_msg.pose.orientation.y = 0.0
+        marker_msg.pose.orientation.z = 0.0
+        marker_msg.pose.orientation.w = 1.0
+
+        marker_msg.scale.x = 0.1  # l'épaisseur de la ligne en mètres
+        marker_msg.color.a = 1.0  # l'opacité du marker, 1.0 signifie complètement opaque
+        marker_msg.color.r = 1.0  # la composante rouge de la couleur
+        marker_msg.color.g = 0.0  # la composante verte de la couleur
+        marker_msg.color.b = 0.0  # la composante bleue de la couleur
+
+        # Définition des points du LineStrip
+        num_points = 100
+        marker_msg.points = [Point() for _ in range(num_points)]
+        for i in range(num_points):
+            marker_msg.points[i].x = i / 10.0
+            marker_msg.points[i].y = 0.5 * math.sin(i / 10.0)  # une fonction sinusoïdale simple
+            marker_msg.points[i].z = 0.0
+
+        self.publisher_.publish(marker_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    sinusoid_surface_marker = SinusoidSurfaceMarker()
-    rclpy.spin(sinusoid_surface_marker)
-    sinusoid_surface_marker.destroy_node()
+    line_strip_publisher = LineStripPublisher()
+    rclpy.spin(line_strip_publisher)
+    line_strip_publisher.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
