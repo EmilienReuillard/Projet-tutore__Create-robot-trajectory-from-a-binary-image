e16d947775732ffae357fe79dd76d7a510d24e46
antoninrnr <antonin.renoir@orange.fr>
2023-02-14 14:56:08 +0100

    hh

--- main.py
+++ main.py
@@ -288,7 +288,7 @@ def main(args=None):
     
     #déclaration de l'élément graph de la classe graph
     graph1 = graph("TTT.png")
-    l = 1.2#selon x
+    l = 1#selon x
     graph1.image2coord(1,l)
     h = graph1.dim_reel_y #selon y
     print(h)
