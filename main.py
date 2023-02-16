5b42d6305919b26a3786545455f20e1fc68343d3
antoninrnr <antonin.renoir@orange.fr>
2023-02-17 00:35:13 +0100

    ii

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ def main(args=None):
     #Partie à utiliser pour former des dessins avec des coins (exemple des lettres)
     # ---------------------------------------------
     l = 1.2#selon x
-    graph1 = graph("./images/TT.png")
+    graph1 = graph("./images/TTT.png")
     graph1.image2coord(pas=1 , fact_echelle= l)
     origin = [-0.6, 0.9]
     h=graph1.dim_reel_y
